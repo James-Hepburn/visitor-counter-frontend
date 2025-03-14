97d966f060fa42181f895ee4e39601eba2196dee
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2025-03-14 12:31:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function update_visitor_count () {
-  var url = 'https://22v3od3qkf.execute-api.us-east-1.amazonaws.com/prod/visitors';
+  var url = 'https://9nqbh63pt4.execute-api.us-east-1.amazonaws.com/Prod/visitors';
   var counter = document.getElementById ("visitor-count");
 
   try {
